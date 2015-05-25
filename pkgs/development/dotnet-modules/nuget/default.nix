8fc43f707c41e045e5d0ff605792a217542fbc4d
obadz <dav-github@odav.org>
2015-05-25 02:56:20 +0100

    NuGet gets a pkg-config file too

--- pkgs/development/dotnet-modules/nuget/default.nix
+++ pkgs/development/dotnet-modules/nuget/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, helperFunctions, makeWrapper, fetchgit, unzip, mono }:
+{ stdenv, helperFunctions, makeWrapper, dotnetbuildhelpers, fetchgit, unzip, mono }:
 
 let
   nugetBinary = fetchgit {
@@ -6,6 +6,7 @@ let
     rev = "da1f2102f8172df6f7a1370a4998e3f88b91c047";
     sha256 = "1hbnckc4gvqkknf8gh1k7iwqb4vdzifdjd19i60fnczly5v8m1c3";
   };
+  version = "2.8.5";
 in
   helperFunctions.runCommand
     "nuget-binary"
@@ -16,4 +17,5 @@ in
       mkdir -p "$out/bin"
       source "${makeWrapper}/nix-support/setup-hook" # Surely there's a better way to do this?
       makeWrapper "${mono}/bin/mono $out/opt/dotnet/nuget-binary/NuGet.exe" "$out/bin/nuget"
+      ${dotnetbuildhelpers}/bin/create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$out/opt/dotnet/nuget-binary/NuGet.Core.dll" "2.8.5"
     ''
