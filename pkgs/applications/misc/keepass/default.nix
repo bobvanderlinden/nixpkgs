c508ae5e1b9f00576b976a7127481fddff3c1931
obadz <dav-github@odav.org>
2015-05-27 16:57:14 +0100

    build KeePass from source

--- pkgs/applications/misc/keepass/default.nix
+++ pkgs/applications/misc/keepass/default.nix
@@ -1,17 +1,28 @@
-{ stdenv, fetchurl, unzip, makeDesktopItem, mono }:
+{ stdenv, fetchurl, makeWrapper, unzip, makeDesktopItem, mono, dotnetbuildhelpers }:
 
 stdenv.mkDerivation rec {
   name = "keepass-${version}";
   version = "2.29";
 
   src = fetchurl {
-    url = "mirror://sourceforge/keepass/KeePass-${version}.zip";
-    sha256 = "16x7m899akpi036c0wlr41w7fz9q0b69yac9q97rqkixb03l4g9d";
+    url = "mirror://sourceforge/keepass/KeePass-${version}-Source.zip";
+    sha256 = "051s0aznyyhbpdbly6h5rs0ax0zvkp45dh93nmq6lwhicswjwn5m";
   };
 
   sourceRoot = ".";
 
-  phases = [ "unpackPhase" "installPhase" ];
+  buildInputs = [
+    unzip
+    mono
+    dotnetbuildhelpers
+    makeWrapper
+  ];
+
+  patches = [ ./keepass.patch ];
+
+  configurePhase = "rm -rvf Build/*";
+
+  buildPhase = "xbuild /p:Configuration=Release";
 
   desktopItem = makeDesktopItem {
     name = "keepass";
@@ -22,23 +33,26 @@ stdenv.mkDerivation rec {
     categories = "Application;Other;";
   };
 
-
   installPhase = ''
+    mkdir -p "$out/opt/dotnet/Keepass/bin"
+    mkdir -p "$out/opt/dotnet/Keepass/lib"
+
+    cp -v Build/KeePass/Release/* "$out/opt/dotnet/Keepass/bin"
+    cp -v Build/KeePassLib/Release/* "$out/opt/dotnet/Keepass/lib"
+
     mkdir -p "$out/bin"
-    echo "${mono}/bin/mono $out/KeePass.exe" > $out/bin/keepass
-    chmod +x $out/bin/keepass
-    echo $out
-    cp -r ./* $out/
+    mkdir -p "$out/lib"
     mkdir -p "$out/share/applications"
+
+    makeWrapper "${mono}/bin/mono $out/opt/dotnet/Keepass/bin/KeePass.exe" "$out/bin/keepass" 
+    create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$out/opt/dotnet/Keepass/lib/KeePassLib.dll"
     cp ${desktopItem}/share/applications/* $out/share/applications
   '';
-
-  buildInputs = [ unzip ];
 
   meta = {
     description = "GUI password manager with strong cryptography";
     homepage = http://www.keepass.info/;
-    maintainers = with stdenv.lib.maintainers; [amorsillo];
+    maintainers = with stdenv.lib.maintainers; [ amorsillo obadz ];
     platforms = with stdenv.lib.platforms; all;
     license = stdenv.lib.licenses.gpl2;
   };
