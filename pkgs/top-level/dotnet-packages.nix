d05a2607c29ceffc55d678248e03754ac40d7afa
obadz <dav-github@odav.org>
2015-05-28 03:45:13 +0100

    Expose FAKE.exe

--- pkgs/top-level/dotnet-packages.nix
+++ pkgs/top-level/dotnet-packages.nix
@@ -11,7 +11,8 @@ let self = _self // overrides; _self = with self; {
     name = "FAKE";
     version = "3.33.0";
     sha256 = "04gllx9d1w8zn9gq9p5k76b79ix07rilk3apdi72dmz6h3yylcdm";
-    dlls = [ "tools/*.dll" ];
+    exes = { "fake" = "tools/FAKE.exe"; };
+    dlls = [ "tools/Fake*.dll" ];
   };
 
   fsCheck = fetchNuGet {
