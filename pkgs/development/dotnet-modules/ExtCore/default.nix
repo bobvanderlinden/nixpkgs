1785e5481c4e50fc6808b78fd53b4bd965d86ded
obadz <dav-github@odav.org>
2015-05-28 18:45:30 +0100

    ExtCore gets mkDotnetDerivation

--- pkgs/development/dotnet-modules/ExtCore/default.nix
+++ pkgs/development/dotnet-modules/ExtCore/default.nix
@@ -1,9 +1,8 @@
-{ stdenv, fetchgit, makeWrapper, mono, dotnetbuildhelpers, fsharp, pkgconfig, dotnetPackages }:
+{ stdenv, fetchgit, mkDotnetDerivation, fsharp, dotnetPackages }:
 
-stdenv.mkDerivation rec {
+mkDotnetDerivation rec {
   baseName = "ExtCore";
   version = "0.8.46";
-  name = "${baseName}-${version}";
 
   src = fetchgit {
     url = "https://github.com/jack-pappas/ExtCore";
@@ -11,44 +10,20 @@ stdenv.mkDerivation rec {
     sha256 = "0ki1ww2ir3yyp5vszqa6ligb81qpgl37h44h0i6qshwvhnaf491h";
   };
 
-  buildInputs = [
-    makeWrapper
-    mono
-    dotnetbuildhelpers
+  extraBuildInputs = [
     fsharp
-    pkgconfig
     dotnetPackages.nUnit
     dotnetPackages.fsCheck
   ];
 
-  configurePhase = ''
-     # Just to make sure there's no attempt to call these executables
-     sed -i -e 's,mono --runtime=.* \$(.*),true,g' .nuget/NuGet.targets
-     rm -vf .nuget/NuGet.exe # Just to be sure
-     touch  .nuget/NuGet.exe # File needs to be there
-
-     # Fix case
-     sed -i -e s,nuget.targets,NuGet.targets, ExtCore.Tests/ExtCore.Tests.fsproj
-
-     # Fix targets
-     find -name \*.fsproj | xargs -n 1 patch-fsharp-targets.sh
+  postConfigure = ''
+    # Fix case
+    sed -i -e s,nuget.targets,NuGet.targets, ExtCore.Tests/ExtCore.Tests.fsproj
   '';
 
-  buildPhase = ''
-    export FSharpTargetsPath="${fsharp}/lib/mono/4.0/Microsoft.FSharp.Targets"
-    xbuild /p:Configuration="Release (net45)"
-  '';
-
-  installPhase = ''
-    mkdir -p "$out"/opt/dotnet/${baseName}
-    cp -rv ExtCore/bin/net45/Release/* "$out"/opt/dotnet/${baseName}
-    for dll in "$out"/opt/dotnet/${baseName}/ExtCore*.dll
-    do
-      create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$dll"
-    done
-  '';
-
-  dontStrip = true;
+  xBuildFlags = [ "/p:Configuration=\"Release (net45)\"" ];
+  outputFiles = [ "ExtCore/bin/net45/Release/*" ];
+  dllFiles = [ "ExtCore*.dll" ];
 
   meta = {
     description = "ExtCore is an extended core library for F#";
