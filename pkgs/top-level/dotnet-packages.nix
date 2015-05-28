4e7c24e237acd03eecd3551c8d87014264eeb81d
obadz <dav-github@odav.org>
2015-05-28 03:51:21 +0100

    Comment out packages fetched from NuGet that we don't actually use

--- pkgs/top-level/dotnet-packages.nix
+++ pkgs/top-level/dotnet-packages.nix
@@ -22,60 +22,60 @@ let self = _self // overrides; _self = with self; {
     dlls = [ "lib/net45/*.dll" ];
   };
 
-  fsCheckNunit = fetchNuGet {
-    name = "FsCheck.Nunit";
-    version = "1.0.4";
-    sha256 = "1s62jrsa5hxqy1ginl8r29rjdc8vbkwmz7mb0hglhwccdqfyr5xy";
-    dlls = [ "lib/net45/*.dll" ];
-  };
+  # fsCheckNunit = fetchNuGet {
+  #   name = "FsCheck.Nunit";
+  #   version = "1.0.4";
+  #   sha256 = "1s62jrsa5hxqy1ginl8r29rjdc8vbkwmz7mb0hglhwccdqfyr5xy";
+  #   dlls = [ "lib/net45/*.dll" ];
+  # };
 
-  fsharpCompilerService = fetchNuGet {
-    name = "FSharp.Compiler.Service";
-    version = "0.0.89";
-    sha256 = "1zl0n2w8fgssi0ajyca1cp2c8a3ysxvjnf697i17asch782mdhrf";
-    dlls = [ "lib/net45/*.dll" ];
-  };
+  # fsharpCompilerService = fetchNuGet {
+  #   name = "FSharp.Compiler.Service";
+  #   version = "0.0.89";
+  #   sha256 = "1zl0n2w8fgssi0ajyca1cp2c8a3ysxvjnf697i17asch782mdhrf";
+  #   dlls = [ "lib/net45/*.dll" ];
+  # };
 
-  fsharpFormatting = fetchNuGet {
-    name = "FSharp.Formatting";
-    version = "2.9.6";
-    sha256 = "190myfj50fl0xs35xf5cz9v99jgwz3v5x5cx1zzwi8fqgym2l7qr";
-    dlls = [ "lib/net40/*.dll" ];
-  };
+  # fsharpFormatting = fetchNuGet {
+  #   name = "FSharp.Formatting";
+  #   version = "2.9.6";
+  #   sha256 = "190myfj50fl0xs35xf5cz9v99jgwz3v5x5cx1zzwi8fqgym2l7qr";
+  #   dlls = [ "lib/net40/*.dll" ];
+  # };
 
-  fsharpVSPowerToolsCore = fetchNuGet {
-    name = "FSharpVSPowerTools.Core";
-    version = "1.8.0";
-    sha256 = "1jmdk8ff07fm1jr18igfixqj5blvcyfwi9lm03yncmnz0mxpv0zh";
-    dlls = [ "lib/net45/*.dll" ];
-  };
+  # fsharpVSPowerToolsCore = fetchNuGet {
+  #   name = "FSharpVSPowerTools.Core";
+  #   version = "1.8.0";
+  #   sha256 = "1jmdk8ff07fm1jr18igfixqj5blvcyfwi9lm03yncmnz0mxpv0zh";
+  #   dlls = [ "lib/net45/*.dll" ];
+  # };
 
-  ilRepack = fetchNuGet {
-    name = "ILRepack";
-    version = "1.26.0";
-    sha256 = "1csx8qjj5w6i9v75qd8bz1j5c86lmg20nf3r47lasbgbw316j33v";
-  };
+  # ilRepack = fetchNuGet {
+  #   name = "ILRepack";
+  #   version = "1.26.0";
+  #   sha256 = "1csx8qjj5w6i9v75qd8bz1j5c86lmg20nf3r47lasbgbw316j33v";
+  # };
 
-  microsoftBclBuild = fetchNuGet {
-    name = "Microsoft.Bcl.Build";
-    version = "1.0.21";
-    sha256 = "0w08d8p87z3v3z28kz92xm19by6jgm6qrlh6pyl80imlg7mkmx61";
-    dlls = [ "build/*.dll" ];
-  };
+  # microsoftBclBuild = fetchNuGet {
+  #   name = "Microsoft.Bcl.Build";
+  #   version = "1.0.21";
+  #   sha256 = "0w08d8p87z3v3z28kz92xm19by6jgm6qrlh6pyl80imlg7mkmx61";
+  #   dlls = [ "build/*.dll" ];
+  # };
 
-  microsoftBcl = fetchNuGet {
-    name = "Microsoft.Bcl";
-    version = "1.1.10";
-    sha256 = "198ji76nz3738f20k8qkqa5gxicbmpl1x5xzisrkskncy7s801i2";
-    dlls = [ "lib/net40/*.dll" ];
-  };
+  # microsoftBcl = fetchNuGet {
+  #   name = "Microsoft.Bcl";
+  #   version = "1.1.10";
+  #   sha256 = "198ji76nz3738f20k8qkqa5gxicbmpl1x5xzisrkskncy7s801i2";
+  #   dlls = [ "lib/net40/*.dll" ];
+  # };
 
-  microsoftNetHttp = fetchNuGet {
-    name = "Microsoft.Net.Http";
-    version = "2.2.29";
-    sha256 = "079qh7wsqmzd7gyj2f9x9apy3vam84jk5i5hrf5cjnyyilbla19h";
-    dlls = [ "lib/net40/*.dll" ];
-  };
+  # microsoftNetHttp = fetchNuGet {
+  #   name = "Microsoft.Net.Http";
+  #   version = "2.2.29";
+  #   sha256 = "079qh7wsqmzd7gyj2f9x9apy3vam84jk5i5hrf5cjnyyilbla19h";
+  #   dlls = [ "lib/net40/*.dll" ];
+  # };
 
   nUnit = fetchNuGet {
     name = "NUnit";
@@ -99,12 +99,12 @@ let self = _self // overrides; _self = with self; {
     };
   };
 
-  octokit = fetchNuGet {
-    name = "Octokit";
-    version = "0.12.0";
-    sha256 = "1nadnfx4ngz880svjik9pmarbaqgdm2siidzk4q4majr0hinj642";
-    dlls = [ "lib/net45/*.dll" ];
-  };
+  # octokit = fetchNuGet {
+  #   name = "Octokit";
+  #   version = "0.12.0";
+  #   sha256 = "1nadnfx4ngz880svjik9pmarbaqgdm2siidzk4q4majr0hinj642";
+  #   dlls = [ "lib/net45/*.dll" ];
+  # };
 
   unionArgParser = fetchNuGet {
     name = "UnionArgParser";
@@ -113,35 +113,35 @@ let self = _self // overrides; _self = with self; {
     dlls = [ "lib/net40/*.dll" ];
   };
 
-  systemWebMvcExtensions = fetchNuGet {
-    name = "System.Web.Mvc.Extensions.Mvc.4";
-    version = "1.0.9";
-    sha256 = "19wi662m8primpimzifv8k560m6ymm73z0mf1r8ixl0xqag1hx6j";
-  };
+  # systemWebMvcExtensions = fetchNuGet {
+  #   name = "System.Web.Mvc.Extensions.Mvc.4";
+  #   version = "1.0.9";
+  #   sha256 = "19wi662m8primpimzifv8k560m6ymm73z0mf1r8ixl0xqag1hx6j";
+  # };
 
-  microsoftAspNetMvc = fetchNuGet {
-    name = "Microsoft.AspNet.Mvc";
-    version = "5.2.2";
-    sha256 = "1jwfmz42kw2yb1g2hgp2h34fc4wx6s8z71da3mw5i4ivs25w9n2b";
-  };
+  # microsoftAspNetMvc = fetchNuGet {
+  #   name = "Microsoft.AspNet.Mvc";
+  #   version = "5.2.2";
+  #   sha256 = "1jwfmz42kw2yb1g2hgp2h34fc4wx6s8z71da3mw5i4ivs25w9n2b";
+  # };
 
-  microsoftAspNetRazor = fetchNuGet {
-    name = "Microsoft.AspNet.Razor";
-    version = "3.2.2";
-    sha256 = "1db3apn4vzz1bx6q5fyv6nyx0drz095xgazqbw60qnhfs7z45axd";
-  };
+  # microsoftAspNetRazor = fetchNuGet {
+  #   name = "Microsoft.AspNet.Razor";
+  #   version = "3.2.2";
+  #   sha256 = "1db3apn4vzz1bx6q5fyv6nyx0drz095xgazqbw60qnhfs7z45axd";
+  # };
 
-  microsoftAspNetWebPages = fetchNuGet {
-    name = "Microsoft.AspNet.WebPages";
-    version = "3.2.2";
-    sha256 = "17fwb5yj165sql80i47zirjnm0gr4n8ypz408mz7p8a1n40r4i5l";
-  };
+  # microsoftAspNetWebPages = fetchNuGet {
+  #   name = "Microsoft.AspNet.WebPages";
+  #   version = "3.2.2";
+  #   sha256 = "17fwb5yj165sql80i47zirjnm0gr4n8ypz408mz7p8a1n40r4i5l";
+  # };
 
-  microsoftWebInfrastructure = fetchNuGet {
-    name = "Microsoft.Web.Infrastructure";
-    version = "1.0.0.0";
-    sha256 = "1mxl9dri5729d0jl84gkpqifqf4xzb6aw1rzcfh6l0r24bix9afn";
-  };
+  # microsoftWebInfrastructure = fetchNuGet {
+  #   name = "Microsoft.Web.Infrastructure";
+  #   version = "1.0.0.0";
+  #   sha256 = "1mxl9dri5729d0jl84gkpqifqf4xzb6aw1rzcfh6l0r24bix9afn";
+  # };
 
   fsUnit = fetchNuGet {
     name = "FsUnit";
