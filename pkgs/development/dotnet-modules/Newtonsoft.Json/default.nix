3cf408d332893c09d6f9b1b024b3f4926c29790d
obadz <dav-github@odav.org>
2015-05-28 18:23:29 +0100

    Newtonsoft.Json gets mkDotnetDerivation

--- pkgs/development/dotnet-modules/Newtonsoft.Json/default.nix
+++ pkgs/development/dotnet-modules/Newtonsoft.Json/default.nix
@@ -1,22 +1,17 @@
-{ stdenv, fetchurl, makeWrapper, mono, dotnetbuildhelpers, fsharp, pkgconfig, dotnetPackages }:
+{ stdenv, fetchurl, mkDotnetDerivation, fsharp, dotnetPackages }:
 
-stdenv.mkDerivation rec {
+mkDotnetDerivation rec {
   baseName = "Newtonsoft.Json";
   version = "6.0.8";
-  name = "${baseName}-${version}";
 
   src = fetchurl {
-    name = "${name}.tar.gz";
+    name = "${baseName}-${version}.tar.gz";
     url = "https://github.com/JamesNK/Newtonsoft.Json/archive/${version}.tar.gz";
     sha256 = "14znf5mycka578bxjnlnz6a3f9nfkc682hgmgg42gdzksnarvhlm";
   };
 
-  buildInputs = [
-    makeWrapper
-    mono
-    dotnetbuildhelpers
+  extraBuildInputs = [
     fsharp
-    pkgconfig
     dotnetPackages.nUnit
     dotnetPackages.systemCollectionsImmutable
     dotnetPackages.autofac
@@ -24,25 +19,14 @@ stdenv.mkDerivation rec {
 
   patches = [ ./references.patch ];
 
-  configurePhase = ''
+  postConfigure = ''
      # Just to make sure there's no attempt to call these executables
      rm -rvf Tools
   '';
 
-  buildPhase = ''
-    xbuild Src/Newtonsoft.Json.sln /p:Configuration=Release
-  '';
-
-  installPhase = ''
-    mkdir -p "$out"/opt/dotnet/${baseName}
-    cp -rv Src/Newtonsoft.Json/bin/Release/* "$out"/opt/dotnet/${baseName}
-    for dll in "$out"/opt/dotnet/${baseName}/Net45/*.dll 
-    do
-      create-pkg-config-for-dll.sh "$out/lib/pkgconfig" "$dll"
-    done
-  '';
-
-  dontStrip = true;
+  xBuildFiles = [ "Src/Newtonsoft.Json.sln" ];
+  outputFiles = [ "Src/Newtonsoft.Json/bin/Release/*" ];
+  dllFiles = [ "Net45/*.dll" ];
 
   meta = {
     description = "Popular high-performance JSON framework for .NET";
