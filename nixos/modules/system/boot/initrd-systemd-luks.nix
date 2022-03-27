f093e6d44470aec1a4b5464bd50e528e1e2707c3
Bob van der Linden <bobvanderlinden@gmail.com>
2022-03-27 20:11:25 +0200

    nixos: systemd: initrd: add luks support

---
+++ nixos/modules/system/boot/initrd-systemd-luks.nix
@@ -0,0 +1,49 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  crypttab =
+    let
+      luksDevices = attrValues config.boot.initrd.luks.devices;
+      stringifyLuksDevice = { name, device, header, keyFile, keyFileSize, keyFileOffset, allowDiscards, ... }@fs:
+        let
+          opts =
+            optional allowDiscards "discard" ++
+            optional (keyFileOffset != null) "keyfile-offset=${keyFileOffset}" ++
+            optional (keyFileSize != null) "keyfile-size=${keyFileSize}" ++
+            optional (header != null) "header=${header}";
+          optsSection = lib.concatStringsSep "," opts;
+          keyFileSection = if keyFile == null then "-" else keyFile;
+        in "${name} ${device} ${keyFileSection} ${optsSection}";
+      content = lib.concatMapStringsSep "\n" stringifyLuksDevice luksDevices;
+    in pkgs.writeText "crypttab" content;
+in
+{
+  config = {
+    boot.initrd.kernelModules =
+      ["dm_mod" "dm_crypt" "cryptd" "input_leds"]
+      ++ config.boot.initrd.luks.cryptoModules ++ [
+        "ecb"
+      ];
+    boot.initrd.systemd = {
+      enable = true;
+      package = pkgs.systemd;
+      additionalUpstreamUnits = [
+        "cryptsetup-pre.target"
+        "cryptsetup.target"
+        "remote-cryptsetup.target"
+      ];
+      emergencyPackages = [
+        pkgs.cryptsetup
+        pkgs.systemd
+        pkgs.util-linux
+      ];
+      objects = [
+        {object = "${pkgs.systemd}/lib/systemd/systemd-cryptsetup";}
+        {
+          object = crypttab;
+          symlink = "/etc/crypttab";
+        }
+      ];
+    };
+  };
+}
